ce321a78216e4c2452206d56cda1103cce7d4e96
Lord Perceval <samsiddinovzieviddin@gmail.com>
2025-05-19 19:07:12 +0500

    1st day tasks

---
+++ day_1/print_input/tasks.py
@@ -0,0 +1,17 @@
+# https://stepik.org/lesson/265077/step/8?unit=246025
+
+# решение задания
+
+print("Здравствуй, мир!")
+
+#https://stepik.org/lesson/265077/step/9?unit=246025
+
+print ("4", "8", "15", "16", "23", "42")
+
+#https://stepik.org/lesson/265077/step/10?unit=246025
+
+print ("4", "8", "15", "16", "23", "42", sep = "\n")
+
+#https://stepik.org/lesson/265077/step/11?unit=246025
+
+print("*", "**", "***", "****", "*****", "******", "*******", sep="\n") 
